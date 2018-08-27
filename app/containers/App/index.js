e276e1aa8623dd2491f230c417fa8927768f3ca4
Runlei He <gilgames.fate@gmail.com>
2018-08-26 23:40:29 -0400

    G-14,15 add group form and event form

--- app/containers/App/index.js
+++ app/containers/App/index.js
@@ -20,7 +20,8 @@ import Header from 'components/Header';
 import Footer from 'components/Footer';
 
 const AppWrapper = styled.div`
-  max-width: calc(768px + 16px * 2);
+  // max-width: calc(768px + 16px * 2);
+  max-width: 70%;
   margin: 0 auto;
   display: flex;
   min-height: 100%;
